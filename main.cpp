55ec81feee34d2655efe4c9616a9137737e5dbc2
Gaurav <gauravkrdas19@gmail.com>
2024-07-03 22:17:02 +0530

    string input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-int main(){
+class RawString{
+private:
+    string rawString;
+    int stringLength;
+public:
+    RawString(string rawString){
+        this->rawString = rawString;
+        this->stringLength = rawString.length();
+    }
+    void getInfo(){
+        cout<<stringLength;
+    }
+};
 
+int main(){
+    string input;
+    getline(cin,input);
+
+    RawString rs(input);
+    rs.getInfo();
+    
     return 0;
 }
