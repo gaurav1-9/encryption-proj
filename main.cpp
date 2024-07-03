20eb1c81a4ec6cd140f50697836b22fb85e2ffe6
Gaurav <gauravkrdas19@gmail.com>
2024-07-03 22:47:42 +0530

    converted to lowercase

--- main.cpp
+++ main.cpp
@@ -4,15 +4,38 @@ using namespace std;
 
 class RawString{
 private:
+    bool isConverted = false;
+protected:
     string rawString;
     int stringLength;
 public:
     RawString(string rawString){
         this->rawString = rawString;
         this->stringLength = rawString.length();
+        toLower();
     }
-    void getInfo(){
-        cout<<stringLength;
+    void toLower(){
+        for(int i=0;i<stringLength;i++){
+            if(rawString[i] == ' ') continue;
+            else{
+                if(rawString[i]>=65 && rawString[i]<=90){
+                    rawString[i] = rawString[i]+32;
+                    isConverted = true;
+                }
+            }
+        }
+        if(isConverted) cout<<endl<<"Input string converted to lower case"<<endl;
+    }
+};
+class CeasarCipher : protected RawString{
+public:
+    CeasarCipher(string rawString):RawString(rawString){}
+
+    void ceasarCipherAlgo(){
+        for(int i=0;i<stringLength;i++){
+            if(rawString[i] == ' ') continue;
+            else cout<<(int)rawString[i]<<endl;
+        }
     }
 };
 
@@ -20,8 +43,8 @@ int main(){
     string input;
     getline(cin,input);
 
-    RawString rs(input);
-    rs.getInfo();
+    CeasarCipher cc(input);
+    cc.ceasarCipherAlgo();
     
     return 0;
 }
